e74fca220b2f938ed8f2268fc8fdf32605f7e6d5
tala <Hethnawi.tala@gmail.com>
2020-02-08 22:49:00 +0200

    init

---
+++ src/main/java/Plus.java
@@ -0,0 +1,13 @@
+public class Plus {
+
+   
+    public static int sum(int x , int y ){
+        return x+y;
+    } 
+    
+    
+    public static void main( String[] aa){
+        System.out.println("Plus");
+    }
+    
+}
